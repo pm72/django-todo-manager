cd721a821b08c3c05e820ab43d947e1b192ce9e1
pm72 <46081859+pm72@users.noreply.github.com>
2025-11-21 15:58:41 +0400

    pm72

--- conf/urls.py
+++ conf/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('', TemplateView)
+  path('admin/', admin.site.urls),
 ]
